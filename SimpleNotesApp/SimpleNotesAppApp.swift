c33db9376a6c4452f8b55dd450792229c250d914
iOS developer K <iOSdvlprK@gmail.com>
2023-07-06 19:21:16 +0900

    Initial Commit

---
+++ SimpleNotesApp/SimpleNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNotesAppApp.swift
+//  SimpleNotesApp
+//
+//  Created by joe on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
